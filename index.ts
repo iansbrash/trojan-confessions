0c1ac5f713923c8b50c279d06a69a55898c2f88e
Ian Brash <brash@usc.edu>
2021-05-25 16:12:58 -0500

    heroku plz

--- index.ts
+++ index.ts
@@ -58,6 +58,10 @@ firebase.initializeApp(firebaseConfig);
 // enable in prod, can't use rn because localhost:3000/5000
 // app.use(cors());
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
+
 // router
 app.use('/api/confessions', confessions);
 
@@ -71,9 +75,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-})
+
 
 
 
