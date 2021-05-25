beb612b57ea0b2ca6af50c03114e617c04ad776b
Ian Brash <brash@usc.edu>
2021-05-25 15:56:44 -0500

    Fix app.get() in if statement

--- index.ts
+++ index.ts
@@ -69,11 +69,11 @@ app.use('/api/admin', admin);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
 }
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 
 
 
