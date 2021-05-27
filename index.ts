d662f2dfecaa7fb4c8c0299b869592cd2dd9a777
Ian Brash <brash@usc.edu>
2021-05-27 12:37:53 -0500

    bro 5

--- index.ts
+++ index.ts
@@ -70,10 +70,10 @@ app.use('/api/preview', preview);
 app.use('/api/admin', admin);
 // test
 // if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join('client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 // }
 // bruh
