b2e766bacdc071195292641767ca961702efac23
Ian Brash <brash@usc.edu>
2021-06-09 14:27:37 -0500

    Remove unneccesary routes from App, remove IndividualRoutes besides CatchallRoute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,19 +11,8 @@ import LoadingIndicator from './Components/Home/LoadingIndicator';
 import Confessions from './Components/Confessions/Confessions';
 import Anonymity from './Components/Anonymity/Anonymity';
 import About from './Components/About/About';
-import IMessageRoute from './Components/Preview/IndividualRoutes/IMessageRoute';
-import ZoomRoute from './Components/Preview/IndividualRoutes/ZoomRoute';
-import TinderRoute from './Components/Preview/IndividualRoutes/TinderRoute';
-import EmailRoute from './Components/Preview/IndividualRoutes/EmailRoute';
-import TwitterRoute from './Components/Preview/IndividualRoutes/TwitterRoute';
-import NotesRoute from './Components/Preview/IndividualRoutes/NotesRoute';
-import SnapchatRoute from './Components/Preview/IndividualRoutes/SnapchatRoute';
-import InstagramRoute from './Components/Preview/IndividualRoutes/InstagramRoute';
 
 import CatchallRoute from './Components/Preview/IndividualRoutes/CatchallRoute';
-
-// Header Testing
-import HeaderTest from './Components/HeaderTest/HeaderTest';
 
 // tailwind
 import "tailwindcss/tailwind.css";
@@ -155,46 +144,6 @@ function App() {
           {/* /preview/:theme */}
           <Route path="/preview/:theme" component={CatchallRoute}/>
 
-          {/* /preview/imessage
-          <Route path="/preview/imessage">
-            <IMessageRoute />
-          </Route>
-
-          {/* /preview/imessage */}
-          {/* <Route path="/preview/zoom">
-            <ZoomRoute />
-          </Route> */}
-
-          {/* /preview/tinder */}
-          {/* <Route path="/preview/tinder">
-            <TinderRoute />
-          </Route> */}
-
-          {/* /preview/email */}
-          {/* <Route path="/preview/email">
-            <EmailRoute />
-          </Route> */}
-
-          {/* /preview/twitter */}
-          {/* <Route path="/preview/twitter">
-            <TwitterRoute />
-          </Route> */}
-
-          {/* /preview/snapchat */}
-          {/* <Route path="/preview/snapchat">
-            <SnapchatRoute />
-          </Route> */}
-
-          {/* /preview/notes */}
-          {/* <Route path="/preview/notes">
-            <NotesRoute />
-          </Route> */}
-
-          {/* /preview/notes */}
-          {/* <Route path="/preview/instagram">
-            <InstagramRoute />
-          </Route> */}
-
           {/* /preview */}
           <Route path="/preview">
             <Helmet>
@@ -203,22 +152,11 @@ function App() {
             <Preview />
           </Route>
 
-          {/* /headertest */}
-          <Route path="/headertest">
-            <HeaderTest />
-          </Route>
-
           {/* / */}
           <Route path="/">
             <Helmet>
               <style>{'html {overflow: scroll;overflow-x: hidden;}::-webkit-scrollbar {width: 0;  background: transparent; }::-webkit-scrollbar-thumb {background: #FF0000;}'}</style>
-
               <style>{'body { background-color: #F3F4F6;'}</style>
-              {/* <style>
-                {'html, body {overflow-x: hidden;}body {position: relative}'}
-              </style>
-              <meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0" />
-              <meta name="apple-mobile-web-app-capable" content="yes" />             */}
             </Helmet>
             <Home />
           </Route>
